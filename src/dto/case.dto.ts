de489e7306bd5ed208e7eadc4f9d8b4ccd09cd3e
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-03-04 22:10:36 +0200

    chore: refactored code
    - added case dto
    - implemented method for file writing

---
+++ src/dto/case.dto.ts
@@ -0,0 +1,4 @@
+export class CaseDto {
+  pattern: RegExp;
+  replacement: string;
+}
--- src/markdown-parser.ts
+++ src/markdown-parser.ts
@@ -1,5 +1,10 @@
 import * as fs from 'fs';
 import {MarkdownParserInterface} from './interfaces/markdown-parser.interface';
+import {MarkdownValidator} from './markdown-validator';
+import {CaseDto} from './dto/case.dto';
+import {dirname} from 'path';
+
+const markdownValidator = new MarkdownValidator();
 
 export class MarkdownParser implements MarkdownParserInterface {
   constructor(
@@ -7,10 +12,19 @@ export class MarkdownParser implements MarkdownParserInterface {
     private readonly out?: string
   ) {}
 
-  private readonly cases = [
-    {pattern: /\*\*(.+?)\*\*/g, replacement: '<b>$1</b>'},
-    {pattern: /_(.+?)_/g, replacement: '<i>$1</i>'},
-    {pattern: /`(.+?)`/g, replacement: '<tt>$1</tt>'},
+  private readonly cases: CaseDto[] = [
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g,
+      replacement: '<b>$1</b>',
+    },
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)_(?=\S)(.+?)(?<=\S)_(?=[ ,.:;\n\t]|$)/g,
+      replacement: '<i>$1</i>',
+    },
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)`(?=\S)(.+?)(?=\S)`(?=[ ,.:;\n\t]|$)/g,
+      replacement: '<tt>$1</tt>',
+    },
   ];
 
   private preformattedText: string[] = [];
@@ -18,17 +32,23 @@ export class MarkdownParser implements MarkdownParserInterface {
   parse() {
     const file = this.readFile();
     const formattedText = this.removePreformattedText(file);
-    const html = this.cases.reduce(
-      (acc, cur) => acc.replace(cur.pattern, cur.replacement),
-      formattedText
-    );
+
+    markdownValidator.checkNesting(formattedText, this.cases);
+    const html = this.cases.reduce((acc, cur) => {
+      return acc.replace(cur.pattern, cur.replacement);
+    }, formattedText);
+
     const htmlWithParagraphs = this.setParagraphs(html);
-    console.log(this.setPreformattedText(htmlWithParagraphs));
+    markdownValidator.checkUnpairedMarkup(htmlWithParagraphs);
+
+    const result = this.setPreformattedText(htmlWithParagraphs);
+    this.out ? this.writeFile(result) : console.log(result);
   }
 
   private removePreformattedText(text: string): string {
     const preformattedText = text.match(/```([\s\S]*?)```/g);
     if (!preformattedText) return text;
+
     this.preformattedText.push(...preformattedText);
     return preformattedText.reduce(
       (acc, cur, index) => acc.replace(cur, `PRE{{${index}}}PRE`),
@@ -48,6 +68,15 @@ export class MarkdownParser implements MarkdownParserInterface {
   }
 
   private readFile(): string {
+    const isExists = fs.existsSync(this.path);
+    if (!isExists) throw new Error('file not found');
     return fs.readFileSync(this.path, 'utf-8');
+  }
+
+  private writeFile(text: string) {
+    const path = dirname(this.out);
+    const isExists = fs.existsSync(path);
+    if (!isExists) throw new Error('out is not correct');
+    fs.writeFileSync(this.out, text);
   }
 }
