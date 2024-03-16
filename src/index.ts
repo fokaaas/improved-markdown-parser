50ccea1d4a029628e32f01dddb2313b96812e265
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-03-16 17:55:13 +0200

    feature: implemented handling of parsers
    - added rule to linter (no-explicit-any)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
+import { MarkdownToEscapeParser } from './parsers/markdown-to-escape.parser';
 import { MarkdownToHtmlParser } from './parsers/markdown-to-html.parser';
 
 const program = new Command();
+
+const parsers: any = {
+  html: MarkdownToHtmlParser,
+  escape: MarkdownToEscapeParser,
+};
 
 program
   .name('MD to HTML parser')
@@ -9,8 +15,26 @@ program
   .version('0.0.1', '-v, --version', 'output the current version')
   .argument('<path>', 'path to markdown file')
   .option('-o, --out <path>', 'path to html file')
+  .addOption(
+    new Option('-f, --format <type>', 'output format').choices([
+      'html',
+      'escape',
+    ])
+  )
   .parse();
 
 const options = program.opts();
-const parser = new MarkdownToHtmlParser(program.args[0], options.out);
-parser.parse();
+
+const parse = (path: string, out?: string) => {
+  if (!options.format) {
+    const parser = options.out
+      ? new MarkdownToHtmlParser(path, out)
+      : new MarkdownToEscapeParser(path);
+    parser.parse();
+  } else {
+    const Parser = parsers[options.format];
+    new Parser(path, out).parse();
+  }
+};
+
+parse(program.args[0], options.out);
