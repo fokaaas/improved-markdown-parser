b96435cd4314dde8ed456e035de6085fe722c422
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-03-16 17:13:11 +0200

    feature: created main class for parsers
    - added cases for html & escape
    - moved removedPreformattedText, readMarkdown, writeResult methods from html parser

--- src/parsers/markdown.parser.ts
+++ src/parsers/markdown.parser.ts
@@ -1,14 +1,43 @@
 import * as fs from 'fs';
 import { dirname } from 'path';
-import { MarkdownParserInterface } from '../interfaces/markdown-parser.interface';
+import { CaseDto } from '../dto/case.dto';
 
-export abstract class MarkdownParser implements MarkdownParserInterface {
+export class MarkdownParser {
   protected constructor(
     private readonly path: string,
     protected readonly out?: string
   ) {}
 
-  abstract parse(): void;
+  protected readonly preformattedText: string[] = [];
+
+  protected readonly cases: CaseDto[] = [
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g,
+      html: '<b>$1</b>',
+      escape: '\x1b[1m$1\x1b[22m',
+    },
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)_(?=\S)(.+?)(?<=\S)_(?=[ ,.:;\n\t]|$)/g,
+      html: '<i>$1</i>',
+      escape: '\x1b[3m$1\x1b[23m',
+    },
+    {
+      pattern: /(?<=[ ,.:;\n\t]|^)`(?=\S)(.+?)(?=\S)`(?=[ ,.:;\n\t]|$)/g,
+      html: '<tt>$1</tt>',
+      escape: '\x1b[7m$1\x1b[27m',
+    },
+  ];
+
+  protected removePreformattedText(text: string): string {
+    const preformattedText = text.match(/```([\s\S]*?)```/g);
+    if (!preformattedText) return text;
+
+    this.preformattedText.push(...preformattedText);
+    return preformattedText.reduce(
+      (acc, cur, index) => acc.replace(cur, `PRE{{${index}}}PRE`),
+      text
+    );
+  }
 
   protected readMarkdown(): string {
     const isExists = fs.existsSync(this.path);
